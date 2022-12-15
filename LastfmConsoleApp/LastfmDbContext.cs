23146a3758fc0879a5cf80d20304db5ba42016fd
Комаров Константин Алексеевич <KAKomarov@alfabank.ru>
2022-12-15 16:53:06 +0300

    Initial

---
+++ LastfmConsoleApp/LastfmDbContext.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LastfmConsoleApp;
+
+public class LastfmDbContext : DbContext
+{
+    public DbSet<LastTrackDto> Scrobbles { get; set; }
+
+    public DbSet<LastAlbumDto> Albums { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable("CONNECTION_STRING"));
+        optionsBuilder.UseSnakeCaseNamingConvention();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<LastAlbumDto>()
+            .Property(p => p.ReleaseDate)
+            .HasColumnType("date");
+
+        modelBuilder.Entity<LastTrackDto>()
+            .Property(p => p.ArtistMbid)
+            .IsRequired(false);
+        modelBuilder.Entity<LastTrackDto>()
+            .Property(p => p.AlbumMbid)
+            .IsRequired(false);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
+
+[Table("scrobbles")]
+public class LastTrackDto
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Mbid { get; set; }
+
+    public string ArtistName { get; set; }
+
+    public string ArtistMbid { get; set; }
+
+    public Uri Url { get; set; }
+
+    // public LastImageSet Images { get; set; }
+
+    public string AlbumName { get; set; }
+
+    public string AlbumMbid { get; set; }
+
+    // public IEnumerable<LastTag> TopTags { get; set; }
+
+    public DateTimeOffset? TimePlayed { get; set; }
+}
+
+public class LastAlbumDto
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+
+    public string ArtistName { get; set; }
+
+    public DateTime ReleaseDate { get; set; }
+
+    public int? Year { get; set; }
+
+    public string Mbid { get; set; }
+
+    public Uri Url { get; set; }
+}
---
+++ LastfmConsoleApp/Program.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using IF.Lastfm.Core.Api;
+using IF.Lastfm.Core.Api.Helpers;
+using IF.Lastfm.Core.Objects;
+using LastfmConsoleApp;
+
+var client = new LastfmClient(Environment.GetEnvironmentVariable("LASTFM_API_KEY"), Environment.GetEnvironmentVariable("LASTFM_API_SECRET"));
+var config = new MapperConfiguration(cfg => cfg.CreateMap<LastTrack, LastTrackDto>());
+var mapper = config.CreateMapper();
+
+
+var userName = "MolinRE";
+var offset = new DateTime(2021, 12, 1);
+var pageNumber = 1;
+var count = 200;
+Console.WriteLine($"GetRecentScrobbles: userName = {userName}, offset = {offset}, pageNumber = {pageNumber}, count = {count}");
+var scrobbles = await Load(pageNumber);
+
+// TODO: mbid null
+// TODO: После добавления новых скробблингов добавить новые альбомы
+// Во время добавления альбомов чистить mater title (deluxe edition и т.д.)
+
+await using var db = new LastfmDbContext();
+
+// db.Database.EnsureDeleted();
+db.Database.EnsureCreated();
+
+Console.WriteLine("На данный момент код умеет только загружать скробблинги с определенной даты.");
+
+return;
+
+Add(scrobbles.Content);
+
+while (pageNumber < scrobbles.TotalPages)
+{
+    pageNumber++;
+    scrobbles = await Load(pageNumber);
+    Add(scrobbles.Content);
+}
+
+Console.WriteLine("Сохранение скробблингов в БД...");
+await db.SaveChangesAsync();
+Console.WriteLine("Готово");
+
+void Add(IReadOnlyList<LastTrack> content)
+{
+    foreach (var scrobble in content)
+    {
+        var dto = mapper.Map<LastTrackDto>(scrobble);
+        db.Scrobbles.Add(dto);
+    }
+}
+
+async Task<PageResponse<LastTrack>> Load(int page)
+{
+    var scrobbles = await client.User.GetRecentScrobbles(userName, offset, page, count);
+    Console.WriteLine($"Страница {scrobbles.Page}/{scrobbles.TotalPages}, размер = {scrobbles.PageSize}");
+
+    return scrobbles;
+}
+
